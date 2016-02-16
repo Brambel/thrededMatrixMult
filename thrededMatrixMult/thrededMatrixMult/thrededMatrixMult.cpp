390b64eceff09a5373fd5ab8dd5edb0491f24f40
Brambel <jamesebert1@gmail.com>
2016-02-16 07:47:13 -0600

    another ignore update

--- thrededMatrixMult/thrededMatrixMult/thrededMatrixMult.cpp
+++ thrededMatrixMult/thrededMatrixMult/thrededMatrixMult.cpp
@@ -2,10 +2,22 @@
 //
 
 #include "stdafx.h"
+#include <pthread.h>
 
+int ** addMtx(int**, int**);
+int** multMtx(int**, int**);
 
 int main()
 {
     return 0;
 }
 
+int** addMtx(int** a, int** b){
+
+	return 0;
+}
+
+int** multMtx(int**, int**) {
+
+	return 0;
+}
