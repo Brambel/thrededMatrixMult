5fb67b8b1514f91062903ebf8b579a9e34efff24
Brambel <Brambel@users.noreply.github.com>
2016-02-17 08:18:20 -0600

    save point
    need to figure out what add is going to return

--- thrededMatrixMult/thrededMatrixMult.cpp
+++ thrededMatrixMult/thrededMatrixMult.cpp
@@ -1,14 +1,6 @@
 
-
 #include <pthread.h>
 #include <iostream>
-
-struct multArgs{
-  int** c;
-  int** a;
-  int** b;
-  int n;
-};
 
 struct subArys{
   int** ary11;
@@ -17,6 +9,24 @@ struct subArys{
   int** ary22;
   int n;  
 };
+
+struct multArgs{
+  int** c;
+  int** a;
+  int** b;
+  int n;
+  
+  public:
+    multArgs(int** c, int** a, int** b, int n){
+      c=c;
+      a=a;
+      b=b;
+      n=n;
+    }
+  
+};
+
+
 
 void *multMtx(int**, int**, int**, int);
 void *addMtx(int**, int**, int);
@@ -37,30 +47,57 @@ int main()
 
 void *multMtx(void* param){
 	  multArgs* par = (multArgs*) param;
+	  int ** tt = newAry(par->n); //create empty array
 	if(par->n == 1){
-	  par->c[1][1]=par->a[1][1] * par->b[1][1];
+	  par->c[0][0] = par->a[0][0] * par->b[0][0];
+	  //need to bundle for return here
 	}
 	else{
-	  int ** t = newAry(par->n); //create empty array
-	   //multArgs parm1 = {par->c[1][1],par->a[1][1],par->b[1][1], par->n/2};
-	    
+	  
+	  subArys* a = split(par->a, par->n); //aplit arrays
+	  subArys* b = split(par->b, par->n);
+	  subArys* c = split(par->c, par->n);
+	  subArys* t = split(tt, par->n);
+	  //package all params
+	  multArgs* param1 = new multArgs(c->ary11, a->ary11, b->ary11, t->n);
+	  mutArgs* param2 = new multArgs(c->ary12, a->ary11, b->ary12, t->n);
+	  mutArgs* param3 = new multArgs(c->ary21, a->ary21, b->ary11, t->n);
+	  mutArgs* param4 = new multArgs(c->ary22, a->ary21, b->ary12, t->n);
+	  mutArgs* param5 = new multArgs(t->ary11, a->ary12, b->ary22, t->n);
+	  mutArgs* param6 = new multArgs(t->ary12, a->ary1t, b->ary22, t->n);
+	  mutArgs* param7 = new multArgs(t->ary21, a->ary22, b->ary21, t->n);
+	  mutArgs* param8 = new multArgs(t->ary22, a->ary22, b->ary22, t->n);
 	  pthread_t m1, m2, m3, m4, m5, m6, m7, m8;
-	  //pthread_create(m1, NULL, multMtx, (void*) param1);
-	 delete t; 
+	  pthread_create(m1, NULL, multMtx, (void*) param1);
+	  //add and bundle for return here
 	}
 	
+	delete tt;
 	return 0;
 }
 
-void *addMtx(int** a, int** b, int n) {
-
+void *addMtx(void* param) {
+	multArgs* par = (multArgs*) param;
+	int ** tt = newAry(par->n);
+	
+	if(par->n==1){
+	  par->c[0][0]+=par->t[0][0];
+	  //package for return
+	}
+	else{
+	  subArys* c = split(par->c, par->n);
+	  subArys* t = split(tt, par->n);
+	}
 	return 0;
 }
 
 int** newAry(int n){
-  int** temp = new int*[n];
+  int** temp = new int*[n]; //outer array for poitners
   for(int i=0;i<n;++i){
-   temp[i]=new int[n]; 
+   temp[i]=new int[n]; //inner 1d arrays of ints
+   for(int j=0;j<n;++j){
+     temp[i][j]=0; //init to 0
+   }
   }
   return temp;
 }
@@ -68,13 +105,14 @@ int** newAry(int n){
 subArys* split(int** ary, int n){
  subArys* temp = new subArys;
  
- temp->ary11 = newAry(n/2);
+ temp->ary11 = newAry(n/2); //define all initial vals
  temp->ary12 = newAry(n/2);
  temp->ary21 = newAry(n/2);
  temp->ary22 = newAry(n/2);
  temp->n = n/2;
  int shift=n/2;
- for(int i=0;i<n;++i){
+ 
+ for(int i=0;i<n;++i){ //loop through original and push to correct quad
    for(int j=0;j<n;++j){
      if(i<n/2){
 	if(j<n/2){
