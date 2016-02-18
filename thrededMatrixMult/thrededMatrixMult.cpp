384dafc89a69917e760e5665503ae86904f14578
Brambel <Brambel@users.noreply.github.com>
2016-02-18 08:10:35 -0600

    testing for addMtx
    unit test for the addition routine added and passed.

--- thrededMatrixMult/thrededMatrixMult.cpp
+++ thrededMatrixMult/thrededMatrixMult.cpp
@@ -17,13 +17,12 @@ struct multArgs{
   int n;
   
   public:
-    multArgs(int** c, int** a, int** b, int n){
-      c=c;
-      a=a;
-      b=b;
-      n=n;
+    multArgs(int** c_, int** a_, int** b_, int n_){
+      c=c_;
+      a=a_;
+      b=b_;
+      n=n_;
     }
-  
 };
 
 struct addArgs{
@@ -32,28 +31,29 @@ struct addArgs{
   int n;
   
   public:
-    addArgs(int** c, int** t, int n){
-      c=c;
-      t=t;
-      n=n;
+    addArgs(int** c_, int** t_, int n_){
+      c=c_;
+      t=t_;
+      n=n_;
     }
 };
 
 
 
-void *multMtx(int**, int**, int**, int);
-void *addMtx(int**, int**, int);
+void *multMtx(void*);
+void *addMtx(void*);
 int** newAry(int);
 subArys* split(int**, int);
+void printAry(int**, int);
 
 void testSplit(int);
+void testAdd();
 
 using namespace std;
 
-int main()
-{
+int main(){
   testSplit(4);
-  
+  testAdd();
   
     return 0;
 }
@@ -80,18 +80,17 @@ void *multMtx(void* param){
 	  multArgs* param6 = new multArgs(t->ary12, a->ary12, b->ary22, t->n);
 	  multArgs* param7 = new multArgs(t->ary21, a->ary22, b->ary21, t->n);
 	  multArgs* param8 = new multArgs(t->ary22, a->ary22, b->ary22, t->n);
-	  pthread_t *m1, *m2, *m3, *m4, *m5, *m6, *m7, *m8;
-	  pthread_create(m1, NULL, multMtx, (void*) param1);
+	  pthread_t m1, m2, m3, m4, m5, m6, m7, m8;
+	  pthread_create(&m1, NULL, multMtx, (void*) param1);
 	  //add and bundle for return here
 	}
 	
 	delete tt;
-	return new void*;
+
 }
 
 void *addMtx(void* param) {
 	addArgs* par = (addArgs*) param;
-	int ** tt = newAry(par->n);
 	
 	if(par->n==1){
 	  par->c[0][0]+=par->t[0][0];
@@ -99,26 +98,26 @@ void *addMtx(void* param) {
 	}
 	else{
 	  subArys* c = split(par->c, par->n);
-	  subArys* t = split(tt, par->n);
+	  subArys* t = split(par->t, par->n);
 	  
 	  addArgs* param1 = new addArgs(c->ary11, t->ary11, t->n);
-	  addArgs* param2 = new addArgs(c->ary21, t->ary21, t->n);
+	  addArgs* param2 = new addArgs(c->ary12, t->ary12, t->n);
 	  addArgs* param3 = new addArgs(c->ary21, t->ary21, t->n);
 	  addArgs* param4 = new addArgs(c->ary22, t->ary22, t->n);
-	  pthread_t *m1,*m2, *m3, *m4;
-	  pthread_t* threads[4]={m1, m2, m3, m4};
-	  
-	  pthread_create(m1, NULL, addMtx, (void*) param1);
-	  pthread_create(m2, NULL, addMtx, (void*) param2);
-	  pthread_create(m3, NULL, addMtx, (void*) param3);
-	  pthread_create(m4, NULL, addMtx, (void*) param4);
+	  pthread_t m1,m2, m3, m4;
+	  pthread_t threads[4]={m1, m2, m3, m4};
+	   
+	  pthread_create(&m1, NULL, addMtx, (void*) param1);
+	  pthread_create(&m2, NULL, addMtx, (void*) param2);
+	  pthread_create(&m3, NULL, addMtx, (void*) param3);
+	  pthread_create(&m4, NULL, addMtx, (void*) param4);
 	  
 	  //thread syntax is all over the place and looks like a toddler threw up in my code
-	  for(int i=0;i<4;++i){
-	   pthread_join(*threads[i],NULL); 
-	  }
+	  pthread_join(m1,NULL);
+	  pthread_join(m2,NULL);
+	  pthread_join(m3,NULL);
+	  pthread_join(m4,NULL);
 	}
-	return new void*;
 }
 
 int** newAry(int n){
@@ -150,15 +149,19 @@ subArys* split(int** ary, int n){
    }
    else{
     temp->ary21[i%shift]=&ary[i][0];
-    temp->ary22[i%shift]=&ary[i][n%shift];
+    temp->ary22[i%shift]=&ary[i][n/shift];
    }
  }
+  if(n==2){//shitty edge case
+    temp->ary12[0]=&ary[0][1];
+    temp->ary22[0]=&ary[1][1];
+  }
    return temp;
 }
 
 void testSplit(int n){
   int** ptr=newAry(n);
-  
+  cout<<"*** Start Split Test ***\n";
   cout<<"parent matrix to be split\n";
   for(int i=0;i<n;++i){
     for(int j=0;j<n;++j){
@@ -207,4 +210,41 @@ void testSplit(int n){
     }
     cout<<endl;
   }
+  cout<<"*** End Split Test ***\n";
+}
+
+void testAdd(){
+  int n=2;
+  int** subA = newAry(n);
+  int** subB = newAry(n);
+  
+  cout<<"*** Start Add Test ***\n";
+  for(int i=0;i<n;++i){
+      for(int j=0;j<n;++j){
+	subA[i][j]=(i*2)+j;
+	subB[i][j]=(i*2)+j;
+      }
+    }
+  printAry(subA, n);
+  printAry(subB, n);
+  
+  addArgs* package = new addArgs(subA, subB, n);
+  
+  addMtx((void*)package);
+  
+  printAry(subA, 2);
+  printAry(subB, 2);
+  
+  cout<<"*** End Add Test ***\n";
+  
+}
+
+void printAry(int ** ary, int n){
+    for(int i=0;i<n;++i){
+      for(int j=0;j<n;++j){
+	cout<<ary[i][j]<<" ";
+      }
+      cout<<endl;
+    }
+    cout<<endl;
 }
